64e94d91b77160bbc04c0021c526e4590e92aec5
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-03 17:03:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function playSound(soundName) {
+    const audio = document.getElementById("audio");
+    audio.src = "sounds/" + soundName;
+    audio.play();
+}
+
+function stopSound() {
+    const audio = document.getElementById("audio");
+    audio.pause();
+}
